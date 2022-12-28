208fbde8bddb867b47a9c1f752d6bd8f4092fdd1
Daniel Vono <sand.doomers@gmail.com>
2022-12-28 12:43:03 -0500

    Added the ability to add new birthdays from the program and created a menu

--- main.py
+++ main.py
@@ -35,6 +35,90 @@ def CreateNewSheet():
     #Close the file
     file.close()
 
+#Adds a new birthday to the birthday sheet
+def AddNewBirthday():
+
+    #Asks the user for the birthday celebrant's name and birth month
+    celebrant_name = input("Enter the name of the birthday celebrant\n")
+    month = input("Enter the month of their birthday\n")
+
+    #If the month is more than 2 characters, treat the month as the word
+    if len(month) > 2:
+
+        #Convert the  month to all upper case
+        month = month.upper()
+
+        #Convert the word into the corresponding number and exit the function if there is no valid month
+        match month:
+            case "JANUARY":
+                month = 1
+            case "FEBRUARY":
+                month = 2
+            case "MARCH":
+                month = 3
+            case "APRIL":
+                month = 4
+            case "MAY":
+                month = 5
+            case "JUNE":
+                month = 6
+            case "JULY":
+                month = 7
+            case "AUGUST":
+                month = 8
+            case "SEPTEMBER":
+                month = 9
+            case "OCTOBER":
+                month = 10
+            case "NOVEMBER":
+                month = 11
+            case "DECEMBER":
+                month = 12
+            case _:
+                print("Error: Invalid month, expected the name of a month")
+                return
+    #Else the month is in numerical form
+    else:
+        #Try to convert the month into an int, this will return an error if a string with a length of 2 is used
+        #NOTE: this will return an incroect value if a single character with an ASCI value of 1-12 is used
+        try:
+            if int(month) <= 0 or int(month) >= 13:
+                print("Error: Invalid month, expected a number between 1 and 12")
+                return
+        except ValueError:
+            print("Error: Invalid month, expected a number between 1 and 12")
+            return
+
+    #Ask the user for the birthday celebrant's day of birth
+    day = input("Enter the day of their birthday\n")
+
+    #Check if the day is a nmuber and exit the program if so
+    try:
+       day = int(day)
+    except ValueError:
+        print("Error: Invalid day, expected a number between 1 and 31")
+        return
+    
+    #Exit the function if the day is not a real date
+    #NOTE: does not account for certian months not having certain days
+    if day <= 0 or day >= 32:
+        print("Error: Invalid day, expected a number between 1 and 31")
+        return
+
+    #Create a tuple to store the birthday information
+    birthday = (month, day, celebrant_name)
+
+    #Write the birthday information to the birthday sheet
+    with open("birthdays.csv", "a") as file:
+        writer = csv.writer(file)
+        writer.writerow(birthday)
+
+    #Print to the user that the birthday has been added successfully
+    print("Successfully added birthday celebrant.")
+
+#Set the application running to true
+running = True
+
 #If a birthday sheet does not exist, create a new blank sheet
 if not os.path.exists("birthdays.csv"):
     CreateNewSheet()
@@ -54,4 +138,22 @@ if len(birthdays_today) == 0:
 elif len(birthdays_today) == 1:
     print("Today's birthday celebrant is", birthdays_today[0], ".")
 else:
-    print("Today's birthday celebrants are", ", ".join(birthdays_today), ".")
+    print("Today's birthday celebrants are", ", ".join(birthdays_today) + ".")
+
+#While the program is running
+while running:
+
+    #Ask the user for what they would like to do next
+    selcetion = input("\nWhat would you like to do?\n0: Exit the program\n1: Add new birthday celebrant\n")
+
+    #Handle the choice the user made
+    match selcetion:
+        case "0":
+            #Set the program to stop running
+            running = False
+        case "1":
+            #Add a new birthday to the birthday sheet
+            AddNewBirthday()
+        case _:
+            #Print unknown input
+            print("Unknown input, please try again")
